31613d9e83c7c53c258468fa3f7eb606b4e491fb
jothipriyadharshanr <joe@geospark.co>
2021-06-29 10:53:31 +0530

    0.0.6: #4 SPM Support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+   name: "Raom",
+   products: [
+       .library(
+           name: "Roam",
+           targets: ["Roam"]),
+   ],
+   targets: [
+       .binaryTarget(
+           name: "Roam",
+           url: "Roam.xcframework.zip",
+           checksum: "ae12720a239c78930e1a7b7b57b16fb92c4e100ca06cfde91ab36688a5f7c36f")
+   ]
+)
