161badbae29339aa97cacb188435ed236bc73e0b
jothipriyadharshanr <joe@geospark.co>
2021-06-29 11:11:30 +0530

    0.0.6: Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -12,7 +11,6 @@ let package = Package(
    targets: [
        .binaryTarget(
            name: "Roam",
-           path: "./Roam.xcframework.zip",
-           checksum: "ae12720a239c78930e1a7b7b57b16fb92c4e100ca06cfde91ab36688a5f7c36f")
+           path: "Roam.xcframework.zip")
    ]
 )
