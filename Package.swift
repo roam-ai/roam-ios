aea71473cb61330c3fd93ed8d246a6b65235bbd9
jothipriyadharshanr <joe@geospark.co>
2021-06-29 11:22:36 +0530

    0.0.6: #6 Update other binary targets

--- Package.swift
+++ Package.swift
@@ -6,11 +6,29 @@ let package = Package(
    products: [
        .library(
            name: "roam-ios",
-           targets: ["Roam"]),
+           targets: ["Roam", "AWSAuthCore", "AWSCognitoIdentityProvider", "AWSCognitoIdentityProviderASF", "AWSCore", "AWSIoT", "AWSMobileClientXCF"]),
    ],
    targets: [
        .binaryTarget(
            name: "Roam",
-           path: "Roam/Roam.xcframework")
+           path: "Roam/Roam.xcframework"),
+       .binaryTarget(
+           name: "AWSAuthCore",
+           path: "Roam/AWSAuthCore.xcframework"),
+       .binaryTarget(
+           name: "AWSCognitoIdentityProvider",
+           path: "Roam/AWSCognitoIdentityProvider.xcframework"),
+       .binaryTarget(
+           name: "AWSCognitoIdentityProviderASF",
+           path: "Roam/AWSCognitoIdentityProviderASF.xcframework"),
+       .binaryTarget(
+           name: "AWSCore",
+           path: "Roam/AWSCore.xcframework"),
+       .binaryTarget(
+           name: "AWSIoT",
+           path: "Roam/AWSIoT.xcframework/"),
+       .binaryTarget(
+           name: "AWSMobileClientXCF",
+           path: "Roam/AWSMobileClientXCF.xcframework"),
    ]
 )
