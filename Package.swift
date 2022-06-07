f644fbe3c7b4e5240d6608bb12c127d1b3f5998d
Jothi Priyadharshan <joe@roam.ai>
2022-06-07 16:23:14 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,26 +6,26 @@ let package = Package(
    products: [
        .library(
            name: "roam-ios",
-           targets: ["Roam", "RMAWSAuthCore", "RMAWSCognitoIdentityProvider", "RMAWSCognitoIdentityProviderASF", "RMAWSCore", "RMAWSIoT", "AWSMobileClientXCF"]),
+           targets: ["Roam", "AWSAuthCore", "AWSCognitoIdentityProvider", "AWSCognitoIdentityProviderASF", "AWSCore", "AWSIoT", "AWSMobileClientXCF"]),
    ],
    targets: [
        .binaryTarget(
            name: "Roam",
            path: "Roam/Roam.xcframework"),
        .binaryTarget(
-           name: "RMAWSAuthCore",
+           name: "AWSAuthCore",
            path: "Roam/AWSAuthCore.xcframework"),
        .binaryTarget(
-           name: "RMAWSCognitoIdentityProvider",
+           name: "AWSCognitoIdentityProvider",
            path: "Roam/AWSCognitoIdentityProvider.xcframework"),
        .binaryTarget(
-           name: "RMAWSCognitoIdentityProviderASF",
+           name: "AWSCognitoIdentityProviderASF",
            path: "Roam/AWSCognitoIdentityProviderASF.xcframework"),
        .binaryTarget(
-           name: "RMAWSCore",
+           name: "AWSCore",
            path: "Roam/AWSCore.xcframework"),
        .binaryTarget(
-           name: "RMAWSIoT",
+           name: "AWSIoT",
            path: "Roam/AWSIoT.xcframework"),
        .binaryTarget(
            name: "AWSMobileClientXCF",
