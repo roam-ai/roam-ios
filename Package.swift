ca6d43f17ad213716cd023ed931d6790fc70bbe2
dinesh-roam <dinesh@roam.ai>
2024-05-13 14:31:53 +0530

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -2,39 +2,54 @@
 import PackageDescription
 
 let package = Package(
-   name: "roam-ios",
-   products: [
-       .library(
-           name: "roam-ios",
-           targets: ["Roam", "AWSAuthCore", "AWSCognitoIdentityProvider", "AWSCognitoIdentityProviderASF", "AWSCore", "AWSIoT", "AWSMobileClientXCF", "MqttCocoaAsyncSocket"]),
-   ],
-   dependencies: [
-           .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6"),
-       ],
-   targets: [
-       .binaryTarget(
-           name: "Roam",
-           path: "Roam/Roam.xcframework"),
-       .binaryTarget(
-           name: "AWSAuthCore",
-           path: "Roam/AWSAuthCore.xcframework"),
-       .binaryTarget(
-           name: "AWSCognitoIdentityProvider",
-           path: "Roam/AWSCognitoIdentityProvider.xcframework"),
-       .binaryTarget(
-           name: "AWSCognitoIdentityProviderASF",
-           path: "Roam/AWSCognitoIdentityProviderASF.xcframework"),
-       .binaryTarget(
-           name: "AWSCore",
-           path: "Roam/AWSCore.xcframework"),
-       .binaryTarget(
-           name: "AWSIoT",
-           path: "Roam/AWSIoT.xcframework"),
-       .binaryTarget(
-           name: "AWSMobileClientXCF",
-           path: "Roam/AWSMobileClientXCF.xcframework"),
-       .binaryTarget(
-           name: "MqttCocoaAsyncSocket",
-           path: "Roam/MqttCocoaAsyncSocket.xcframework"),
-   ]
+    name: "roam-ios",
+    products: [
+        .library(
+            name: "RoamCore",
+            targets: ["Roam", "AWSAuthCore", "AWSCognitoIdentityProvider", "AWSCognitoIdentityProviderASF", "AWSCore", "AWSIoT", "AWSMobileClientXCF"]
+        ),
+        .library(
+            name: "RoamMQTTConnector",
+            targets: ["RoamMQTTConnector", "CocoaMQTT", "MqttCocoaAsyncSocket", "Starscream"]
+        )
+    ],
+    dependencies: [
+        // Add any additional dependencies here
+        //eg: .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Roam",
+            path: "Roam/Roam.xcframework"),
+        .binaryTarget(
+            name: "AWSAuthCore",
+            path: "Roam/AWSAuthCore.xcframework"),
+        .binaryTarget(
+            name: "AWSCognitoIdentityProvider",
+            path: "Roam/AWSCognitoIdentityProvider.xcframework"),
+        .binaryTarget(
+            name: "AWSCognitoIdentityProviderASF",
+            path: "Roam/AWSCognitoIdentityProviderASF.xcframework"),
+        .binaryTarget(
+            name: "AWSCore",
+            path: "Roam/AWSCore.xcframework"),
+        .binaryTarget(
+            name: "AWSIoT",
+            path: "Roam/AWSIoT.xcframework"),
+        .binaryTarget(
+            name: "AWSMobileClientXCF",
+            path: "Roam/AWSMobileClientXCF.xcframework"),
+        .binaryTarget(
+            name: "RoamMQTTConnector",
+            path: "Roam/RoamMQTTConnector.xcframework"),
+        .binaryTarget(
+            name: "CocoaMQTT",
+            path: "Roam/CocoaMQTT.xcframework"),
+        .binaryTarget(
+            name: "MqttCocoaAsyncSocket",
+            path: "Roam/MqttCocoaAsyncSocket.xcframework"),
+        .binaryTarget(
+            name: "Starscream",
+            path: "Roam/Starscream.xcframework"),
+    ]
 )
