22738ce69c7250560c805b3e6c3eff88ef7cfc37
dinesh-roam <134250374+dinesh-roam@users.noreply.github.com>
2024-08-22 16:28:18 +0530

    Updated deployment target for RoamMQTTConnector

--- Roam/RoamMQTTConnector.xcframework/ios-arm64/RoamMQTTConnector.framework/Headers/RoamMQTTConnector-Swift.h
+++ Roam/RoamMQTTConnector.xcframework/ios-arm64/RoamMQTTConnector.framework/Headers/RoamMQTTConnector-Swift.h
@@ -308,6 +308,7 @@ SWIFT_CLASS_NAMED("MqttConnectorLocationData")
 - (nonnull instancetype)initWithEntity:(NSEntityDescription * _Nonnull)entity insertIntoManagedObjectContext:(NSManagedObjectContext * _Nullable)context OBJC_DESIGNATED_INITIALIZER;
 @end
 
+
 @class NSString;
 @class NSDate;
 
